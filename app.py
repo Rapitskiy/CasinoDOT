6a07e202a05acc80c8c182b7855fd2e74010190d
Bystrova Kseniia <bystrovaks@biocad.ru>
2024-09-03 18:17:59 +0300

    final

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import requests
 import json
 global deck
 global remaining
+sumhand = 0
+hand = []
 
 @app.route('/')
 def index():
@@ -12,6 +14,7 @@ def index():
 	
 @app.route('/new_game')
 def get_deck():
+	hand.clear()
 	deck =json.loads(requests.post('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1').text)['deck_id']
 	remaining =json.loads(requests.post('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1').text)['remaining']
 	return render_template('index.html',deck=deck, remaining=remaining)
@@ -21,7 +24,16 @@ def get_card(deck_id):
 	card=json.loads(requests.get('https://deckofcardsapi.com/api/deck/'+deck_id+'/draw/?count=1').text)['cards'][0]
 	card_image=card['images']['png']
 	remaining =json.loads(requests.post('https://deckofcardsapi.com/api/deck/'+deck_id+'/').text)['remaining']
-	return render_template('index.html',card_image=card_image,deck=deck_id, remaining=remaining)
+	if card['value'] == 'KING' or 'JACK' or 'QUEEN':
+		card['value']=10
+	elif card['value'] == 'ACE':
+		card['value']=11
+	else:
+		card['value']=card['value']
+	hand.append(card)
+	sumhand = [i['value'] for i in hand]
+	sumhand = [int(i) for i in sumhand]
+	return render_template('index.html',card_image=card_image,deck=deck_id, remaining=remaining, hand=hand, sumhand=sumhand)
 
 if __name__ == '__main__':
  app.run(debug=True)
